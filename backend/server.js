0aef73c47dd34eed884a249634272b43ccae3cbd
maoescobar0911-star <mao.escobar0911@gmail.com>
2026-04-11 08:18:59 -0500

    Conexión backend con MySQL funcionando

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando 🔥');
+});
+
+app.listen(3000, () => {
+  console.log('🔥 Servidor en http://localhost:3000');
+});
